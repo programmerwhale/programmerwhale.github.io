df1f7b0e89cde92a179d3f13daa049b46aafd057
ZR20040284-井雨姗 <jingyushan@zhongruigroup.com>
2022-09-15 17:19:24 +0800

    test

---
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[],\"git\":{\"updatedTime\":1663232264000,\"contributors\":[{\"name\":\"ZR20040284-井雨姗\",\"email\":\"jingyushan@zhongruigroup.com\",\"commits\":3}]},\"filePathRelative\":\"README.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
